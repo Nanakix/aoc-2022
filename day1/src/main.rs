f3bb3cc402c05fdd595d1f0549e0c0c4d0b705ff
nanakix <goldensun2000@hotmail.fr>
2022-12-01 13:51:04 +0100

    Day2 part 2 DONE

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,17 +6,39 @@ fn main() {
     let file_path = "/home/nanakix/Documents/aoc-2022/day1/input.txt";
     println!("In file {}", file_path);
     let mut elf: u32 = 0; // counting '\n' chars as a number of elves
-    let mut max_elf: u32 = 0; // the elf that carries the most calories
-    let mut max_inventory: u32 = 0; // biggest inventory
+    
+    let mut max_elf_one: u32 = 0; // the elf that carries the most calories
+    let mut max_elf_two: u32 = 0;
+    let mut max_elf_three: u32 = 0;
+    
+    let mut top_one_inventory: u32 = 0; // biggest inventory
+    let mut top_two_inventory: u32 = 0;
+    let mut top_three_inventory: u32 = 0;
+
     let mut inventory: u32 = 0;  // current inventory
 
     if let Ok(lines) = read_lines(file_path) {
         for line in lines {
             let tmp: &String = &line.unwrap();
             if tmp.eq("") {
-                if inventory > max_inventory {
-                    max_inventory = inventory;
-                    max_elf = elf;
+                if inventory > top_one_inventory {
+                    top_three_inventory = top_two_inventory;
+                    max_elf_three = max_elf_two;
+                    top_two_inventory = top_one_inventory;
+                    max_elf_two = max_elf_one;
+                    top_one_inventory = inventory;
+                    max_elf_one = elf;
+                }
+                else if inventory > top_two_inventory {
+                    top_three_inventory = top_two_inventory;
+                    max_elf_three = max_elf_two;
+
+                    top_two_inventory = inventory;
+                    max_elf_two = elf;
+                }
+                else if inventory > top_three_inventory {
+                    top_three_inventory = inventory;
+                    max_elf_three = elf;
                 }
                 inventory = 0; // let's check another elf's backpack
                 elf += 1;
@@ -24,12 +46,21 @@ fn main() {
             else {
                 let itmp: u32 = tmp.parse().unwrap();
                 inventory = inventory + itmp; 
-                println!("adding {:#?} calories to the elf {} inventory", tmp, &elf);
+                // println!("adding {:#?} calories to the elf {} inventory", tmp, &elf);
                 
             }
         }
-        println!("and the best elf is {} carrying {} calories", elf, max_inventory);
+        println!("and the best elf is {} carrying {} calories", elf, top_one_inventory);
+        println!("and the top three elves are {}, {}, {}, carrying a total of {} + {} + {} = {} calories, lembas4life",
+         max_elf_one, max_elf_two, max_elf_three,
+         top_one_inventory, top_two_inventory, top_three_inventory,
+         top_one_inventory + top_two_inventory + top_three_inventory,);
     }
+}
+
+
+fn update_elf(top_number: u32, inventory: u32){
+
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
