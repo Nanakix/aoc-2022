42595d00d980b95bfd050db09fa960adc401e430
nanakix <goldensun2000@hotmail.fr>
2022-12-04 23:49:07 +0100

    Day 3 part 1 done, better

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,16 +9,16 @@ fn main() {
     let file_path = "input";
 
     if let Ok(lines) = read_lines(file_path) {
-        let mut total_priority = 0;
+        let mut total_priority: u32 = 0;
         for line in lines {
             match line {
                 Ok(tmp) => {
-                    total_priority += check_bag(&tmp);
+                    total_priority += check_bag(&tmp) as u32; // p1
                 }
                 Err(_e) => println!("Error reading line")
             }
         }
-        print!("total priority: {}", total_priority);
+        print!("total priority: {}", total_priority); // p1
     }
 }
 
@@ -31,39 +31,22 @@ where P: AsRef<Path>, {
     }
 }       
 
-fn check_bag(tmp: &str) -> u32 {
+fn check_bag(bag: &str) -> u8 {
     // reminder: I checked the input, size of bag is always an even number
     // each bag has two compartments of equal size
-    let compartment_size: usize = tmp.len()/2;
-
-    let mut compart_one: HashSet<char> = HashSet::new();
-    let mut compart_two: HashSet<char> = HashSet::new();
-
-
-    let mut cpt: u32 = 0;
-    for c in tmp.chars() { // let's fill the compartments
-        if cpt < compartment_size.try_into().unwrap() { // unwrap conversion from usize to u32
-            cpt += 1;
-            compart_one.insert(c);
-        }
-        else {
-            compart_two.insert(c);
-        }
-    } // I did not find an elegant way to do this, forgive me senpai
+    let compart_one: HashSet<char> = bag[..bag.len()/2].chars().collect();
+    let compart_two: HashSet<char> = bag[bag.len()/2..].chars().collect();
     find_priority(&compart_one, &compart_two)
 }
 
-fn find_priority(one: &HashSet<char>, two: &HashSet<char>) -> u32{
+fn find_priority(one: &HashSet<char>, two: &HashSet<char>) -> u8{
     // now that we have a sorted bag, let's find the common char for the priority
     let priority: Intersection<char, std::collections::hash_map::RandomState> = one.intersection(two);
     let p: char = priority.last().unwrap().clone(); // awful conversion T_T
-    let res: u32 =  p as u32;
 
-    // priority: a = 1, .. z = 26, A=27, .. Z =52 
-    if p.is_lowercase() {
-        res - 96 // ascii table starts lower case alphabet @97
-    }
-    else {
-        res - 64 + 26 // ascii table starts upper case alphabet @65
+    match p {
+        'a'..='z' => (p as u8) - b'a' + 1,
+        'A'..='Z' => (p as u8) - b'A' + 27,
+        _ => 0
     }
 }
