3cfa2a134a0ace0d305ed06cdca4a8b8c978502e
nanakix <goldensun2000@hotmail.fr>
2022-12-08 00:14:41 +0100

    Day 5 Part 5 DONE (cleaned)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,3 @@
-    // use core::num::dec2flt::parse;
 use std::collections::VecDeque;
 use std::io::{self, BufRead};
 use std::fs::File;
@@ -6,21 +5,12 @@ use std::path::Path;
 
 fn main() {
     let file_path = "input";
-    // find the line of the numbers
-    // let mut nb_stacks = 0;
-    // if let Ok(lines) = read_lines(file_path) {
-    //     let l : String = lines.flat_map(|l| l).find(|line| find_numbers(&line)).unwrap();
-    //     nb_stacks = l.trim().chars().last().unwrap_or_default().to_digit(10).unwrap_or_default();
-    //     // let nb2 = l.split_ascii_whitespace().last().unwrap_or_default().trim().parse::<u32>().unwrap_or_default();
-    // }
-
-    // // 2nd reading: fill the vectors
     let mut v : Vec<VecDeque<u8>> = Vec::new();
     v.resize(9, VecDeque::with_capacity(8));
-    // construct all stacks
     if let Ok(lines) = read_lines(file_path) {
         for (index, l) in lines.flat_map(|l| l).enumerate() {
-            if index < 9 {
+            // construct all stacks
+            if index < 9 { // stacks are written from line 1 to 9 in input, too lazy to fetch it generically
                 for (i, chunk) in l.as_bytes().chunks(4).enumerate() {
                     if chunk[1] != 32 {
                         v[i].push_front(chunk[1]);
@@ -28,20 +18,13 @@ fn main() {
                 }
             }
             else {
-                for vdq in &v {
-                    print!("{:?} ", (&vdq.back().unwrap()));
-                }
-                println!();
-                println!("{:?}", &v);
-                let load: Vec<u8>;
-                load = l.split(" ").skip(1).step_by(2).flat_map(|w| w.parse::<u8>()).collect();
+                // parse the moves
+                let load: Vec<u8> = l.split(" ").skip(1).step_by(2).flat_map(|w| w.parse::<u8>()).collect();
+                // do the moves
                 for _ in 0..load[0] {
                     let tmp = v[(load[1]-1) as usize].pop_back().unwrap_or_default();
                     v[(load[2]-1) as usize].push_back(tmp);
                 }
-                println!();
-                println!("{:?}", &v);
-                println!();
             }
         }
         for vdq in &v {
@@ -58,27 +41,3 @@ where P: AsRef<Path>, {
         Err(error) => panic!("Problem opening the file: {:?}", error),
     }
 }
-
-// fn find_numbers(line: &str) -> bool{
-
-//     // v2
-//     line.chars().any(|c| return c.is_digit(10))
-// }
-    
-    // for c in line.chars()
-    //     if c.is_digit(10)
-    //         return true;
-    // v1
-    //line.chars().map(|c|  return c.is_digit(10)).find(|&b| b).unwrap()
-    
-    // v de merde
-    // for c in line.chars() {
-    //     let tmp = c.to_digit(10);
-    //     match tmp {
-    //         Some(d) => if d == 1 {
-    //             return true;
-    //         },
-    //         N
-    //     }
-    // }
-    // false
