358b122552abe8c9941699294be2bb1ef6298ea5
nanakix <goldensun2000@hotmail.fr>
2022-12-20 22:21:39 +0100

    Day 8 part 1 DONE

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -6,12 +6,20 @@ use std::path::Path;
 fn main() {
     let file_path = "input";
     let mut forest: Vec<Vec<char>> = Vec::new();
+    // let mut visible_trees: Vec<Vec<char>> = Vec::new();
     if let Ok(lines) = read_lines(file_path) {
         for l in lines.flatten() {
             forest.push(l.chars().collect());
         }
     }
-    println!("res: {}", count_visibles(&forest));
+    println!("res: {}", count_visibles(&forest, /*&mut visible_trees*/));
+    // for vl in visible_trees {
+        // for vt in vl {
+            // print!("{}", vt);
+        // }
+        // println("{:?}", vl);
+        // println!();
+    // }
     // println!("res: {}", res);// p1: sum of the dirs of size <= 100000
 }
 
@@ -24,17 +32,19 @@ where P: AsRef<Path>, {
     }
 }
 
-fn count_visibles(forest: &Vec<Vec<char>>) -> u32 {
+fn count_visibles(forest: &Vec<Vec<char>>,  /*visible_trees: &mut Vec<Vec<char>>*/) -> u32 {
     let mut cpt: u32 = 0;
     for (y, r) in forest.into_iter().enumerate() {
 
         for (x, tree) in r.into_iter().enumerate() {
             if y == 0 || x == 0 || x == r.len() - 1 || y == forest.len() - 1 { // if the tree is on the edge of the forest
                 cpt += 1;
-                println!("{}", tree);
+                // visible_trees[y].push(tree.clone());
+                // println!("{}", tree);
             }
             else {
                 // if check_row(forest, x, y, tree) { cpt += 1;}
+                // visible_trees[y].push(' ');
                 if check_column(forest, y, x, tree) { cpt += 1;}
             }
         }
@@ -47,7 +57,7 @@ fn check_column(forest: &Vec<Vec<char>>,  x: usize, y: usize, tree: &char) -> bo
     let mut side: u32 = 4;
     let width = forest[0].len();
     // if forest[0..x+1][y].iter().any(|e| e > tree) {
-    if forest[0..x+1].iter().map(|row| row[y]).any(|e| e >= *tree) {
+    if forest[0..x].iter().map(|row| row[y]).any(|e| e >= *tree) {
         // println!("e: {}, tree: {}", e, tree);
         println!("rip1 petit arbre {}, tu n'es pas le plus grand de ta ligne en partant vers la gauche", tree);
         side -= 1; 
@@ -58,11 +68,11 @@ fn check_column(forest: &Vec<Vec<char>>,  x: usize, y: usize, tree: &char) -> bo
         side -= 1;
     }
     if forest[x][0..y].iter().any(|e| e >= tree) {
-        println!("rip2 petit arbre {}, tu n'es pas le plus grand de ta colonne en partant vers le haut", tree);
+        println!("rip3 petit arbre {}, tu n'es pas le plus grand de ta colonne en partant vers le haut", tree);
         side -= 1;
     }
     if forest[x][y+1..width].iter().any(|e| e >= tree) {
-        println!("rip2 petit arbre {}, tu n'es pas le plus grand de ta ligne en partant vers le bas", tree);
+        println!("rip4 petit arbre {}, tu n'es pas le plus grand de ta ligne en partant vers le bas", tree);
         side -= 1;
     }
     side != 0
@@ -83,16 +93,25 @@ fn check_column(forest: &Vec<Vec<char>>,  x: usize, y: usize, tree: &char) -> bo
 
 #[test]
 fn test_parse() {
-    let expected: u32 = 21;
+    let expected: u32 = 210;
     let mut forest: Vec<Vec<char>> = Vec::new();
+    // let mut visible_trees: Vec<Vec<char>> = Vec::new();
     if let Ok(lines) = read_lines("test1") {
         for l in lines.flatten() {
             forest.push(l.chars().collect());
         }
     }
-    let res: u32 = count_visibles(&forest);
-    println!("res: {}", res);
+    let res: u32 = count_visibles(&forest, /*&mut visible_trees*/);
+    // println!("res: {}", res);
     assert_eq!(expected, res);
+    // for vl in visible_trees {
+        // for vt in vl {
+            // print!("{}", vt);
+        // }
+        // println("{:?}", vl);
+        // j'essaie de représenter visuellement les arbres visibles via visible_trees
+        // println!();
+    // }
 }
 
 // #[test]
