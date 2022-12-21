c7a90a585c3f23509488b2a57bc853943b9bd410
nanakix <goldensun2000@hotmail.fr>
2022-12-21 12:12:48 +0100

    Day 8 part 2 DONE

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -11,7 +11,8 @@ fn main() {
             forest.push(l.chars().collect());
         }
     }
-    println!("res: {}", count_visibles(&forest));
+    // println!("res: {}", count_visibles(&forest));
+    println!("res: {}", count_scenic(&forest));
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -40,6 +41,17 @@ fn count_visibles(forest: &Vec<Vec<char>>) -> u32 {
     cpt
 }
 
+fn count_scenic(forest: &Vec<Vec<char>>) -> u32 {
+    let mut scores: Vec<u32> = Vec::new();
+    for (y, r) in forest.into_iter().enumerate() {
+        for (x, tree) in r.into_iter().enumerate() {
+            scores.push(scenic_score(forest, y, x, tree));
+        }
+    }
+    println!("{:?}", scores);
+    *scores.iter().max().unwrap()
+}
+
 fn check_visible(forest: &Vec<Vec<char>>,  x: usize, y: usize, tree: &char) -> bool {
     let mut side: u32 = 4;
     let width = forest[0].len();
@@ -59,15 +71,106 @@ fn check_visible(forest: &Vec<Vec<char>>,  x: usize, y: usize, tree: &char) -> b
     side != 0
 }
 
+fn scenic_score(forest: &Vec<Vec<char>>, x: usize, y: usize, tree: &char) -> u32 {
+    let mut north_score: u32 = 0;
+    let mut south_score: u32 = 0;
+    let mut east_score: u32 = 0;
+    let mut west_score: u32 = 0;
+    let width = forest[0].len();
+    let mut met: bool = false; // have we met a tree as tall as @param tree
+
+    for (i, t) in forest[0..x].iter().rev().enumerate().map(|(index, row)| (index, row[y])) {
+        if &t >= tree && !met {
+            met = true;
+            // println!("west: i: {}, x: {}", i, x);
+            north_score = (i + 1) as u32;
+        }
+    }
+    if !met { // no taller tree is at the west
+        // println!("no taller tree is at the north");
+        north_score = x as u32;
+    }
+    else {
+        met = false;
+    }
+
+    for (i, t) in forest[x+1..width].iter().enumerate().map(|(index, row)| (index, row[y])) {
+        if &t >= tree && !met {
+            met = true;
+            // println!("eastern trees of t{}: {}", tree, t);
+            // println!("east: t: {}, tree: {}, x:{}, i:{}",t, tree, x, i);
+            // println!("east: i: {}, x: {}", i, x);
+            south_score = (i + 1) as u32; //(x  as i32 - (x+i) as i32).abs() as u32;
+        }
+    }
+    if !met { // no taller tree is at the east
+        // println!("no taller tree is at the south");
+        south_score = (width as i32 - (x + 1) as i32).abs() as u32;
+    }
+    else {
+        met = false;
+    }
+
+    for (i, t) in forest[x][0..y].iter().rev().enumerate() {
+        // println!("western trees of t{}: {}", tree, t);
+        if t >= tree  && !met{
+            met = true;
+            // println!("west: i: {}, x: {}, y: {}", i, x, y);
+            // if i as u32 == 0 { // corner case
+                west_score = (i + 1) as u32;
+            // }
+            // else {
+                // west_score = (y as i32 - i as i32).abs() as u32;
+            // }
+        }
+    }
+    
+    if !met { // no taller tree is at the west
+        // println!("no taller tree is at the west");
+        west_score = y as u32;
+    }
+    else {
+        met = false;
+    }
+    for (i, t) in forest[x][y+1..width].iter().enumerate() {
+        // println!("eastern trees of t{}: {}", tree, t);
+        if t >= tree && !met {
+            met = true;
+            // println!("east: i: {}, x:{}, y: {}", i, x, y);
+            east_score = (i + 1) as u32;
+        }
+    }
+    if !met { // no taller tree is at the east
+        // println!("no taller tree is at the east");
+        east_score = (width as i32 - (y+1) as i32).abs() as u32;
+    }
+    // println!("{}", tree);
+    // println!("n:{} w:{} s:{} e:{}", north_score, west_score, south_score, east_score);
+    north_score * south_score * west_score * east_score
+}
+
+// #[test]
+// fn test_parse() {
+//     let expected: u32 = 21;
+//     let mut forest: Vec<Vec<char>> = Vec::new();
+//     if let Ok(lines) = read_lines("test1") {
+//         for l in lines.flatten() {
+//             forest.push(l.chars().collect());
+//         }
+//     }
+//     let res: u32 = count_visibles(&forest);
+//     assert_eq!(expected, res);
+// }
+
 #[test]
-fn test_parse() {
-    let expected: u32 = 210;
+fn test_scenic_score() {
+    let expected: u32 = 8;
     let mut forest: Vec<Vec<char>> = Vec::new();
     if let Ok(lines) = read_lines("test1") {
         for l in lines.flatten() {
             forest.push(l.chars().collect());
         }
     }
-    let res: u32 = count_visibles(&forest);
+    let res: u32 = count_scenic(&forest);
     assert_eq!(expected, res);
 }
