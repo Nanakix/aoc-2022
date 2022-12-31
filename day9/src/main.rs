f1a528c3c8722ff644bf69449ab687b411f9dac6
nanakix <goldensun2000@hotmail.fr>
2022-12-31 14:55:59 +0100

    Day 9 part 1 clean

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -68,43 +68,34 @@ fn check_positions(head: &mut (i32, i32), tail: &mut (i32, i32), visited_pos: &m
 }
 
 fn parse_ropes(s: String, head: &mut (i32, i32), tail: &mut (i32, i32), visited_pos: &mut HashSet<(i32,i32)>) {
-    let mut sp: Vec<&str> = s.split(" ").collect();
+    let sp: Vec<&str> = s.split(" ").collect();
     let nb: u32 = sp[1].parse().unwrap(); // if it's a number
     // up down Y, right left X
     visited_pos.insert((0,0));
-    match sp[0] { // direction
-        "U" => {
-            for _ in 0..nb {
-                head.1 += 1;
-                println!("Up: HEAD: {:?} tail: {:?}", &head, &tail);  
-                check_positions(head, tail, visited_pos);
-            }
-        },
-        "D" => { 
-            for _ in 0..nb {
-                head.1 -= 1;
-                println!("Down: HEAD: {:?} tail: {:?}", &head, &tail);  
-                check_positions(head, tail, visited_pos);
-            }
-        },
-        "L" => {
-            for _ in 0..nb {
+    for _ in 0..nb {
+        match sp[0] { // direction
+            "U" => {
+                    head.1 += 1;
+                    println!("Up: HEAD: {:?} tail: {:?}", &head, &tail);  
+            },
+            "D" => { 
+                    head.1 -= 1;
+                    println!("Down: HEAD: {:?} tail: {:?}", &head, &tail);  
+            },
+            "L" => {
                 head.0 -= 1;
                 println!("Left: HEAD: {:?} tail: {:?}", &head, &tail);  
-                check_positions(head, tail, visited_pos);
-            }
-        },
-        "R" => { 
-            for _ in 0..nb {
+            },
+            "R" => { 
                 head.0 += 1;
                 println!("Right: HEAD: {:?} tail: {:?}", &head, &tail);  
-                check_positions(head, tail, visited_pos);
+            },
+            _ => {
+                panic!("wtf");
             }
-        },
-        _ => {
-            panic!("wtf");
         }
-    }    
+        check_positions(head, tail, visited_pos);
+    }
 }   
 
 #[test]
@@ -122,3 +113,21 @@ fn test_parse() {
     let res = visited_pos.iter().len() as u32;
     assert_eq!(expected, res);
 }
+
+// #[test]
+// fn test_parse() {
+//     let expected: u32 = 13;
+//     let mut head: (i32, i32) = (0,0);
+//     let mut tail: (i32, i32) = (0,0);
+//     let mut visited_pos: HashSet<(i32, i32)> = HashSet::new();
+//     if let Ok(lines) = read_lines("test2") {
+//         for l in lines.flatten() {
+//             // rendre générique parse_ropes en parcourant pour chaque noeud de la corde, genre 
+//             // on lui envoie noeud et noeud.next et ça fait tout les mouvements pour chaque noeud de la corde. 
+//             // parse_ropes(l, &mut head, &mut tail, &mut visited_pos)
+//         }
+//     }
+//     println!("res: {}", visited_pos.iter().len() );// p1: number of visited pos by the tail
+//     let res = visited_pos.iter().len() as u32;
+//     assert_eq!(expected, res);
+// }
