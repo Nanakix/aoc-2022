4f82413e7df4acf64e1ec3a8930544a47fa7accc
nanakix <goldensun2000@hotmail.fr>
2022-12-31 18:19:11 +0100

    Day 9 part 2 DONE

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,14 +6,13 @@ use std::path::Path;
 
 fn main() {
     let file_path = "input";
-    let mut head: (i32, i32) = (0,0); // (X, Y) of the head of the rope
-    let mut tail: (i32, i32) = (0,0);
     let mut visited_pos: HashSet<(i32,i32)> = HashSet::new();
+    let mut knots: Vec<(i32, i32)> = vec![(0,0);10];
     // let mut directions: HashSet<String, u32> = HashSet::new();
     
     if let Ok(lines) = read_lines(file_path) {
         for l in lines.flatten() {
-            parse_ropes(l, &mut head, &mut tail, &mut visited_pos);
+            parse_ropes(l, &mut knots, &mut visited_pos);
         }
     }
         println!("res: {}", visited_pos.iter().len() );// p1: number of visited pos by the tail
@@ -28,46 +27,69 @@ where P: AsRef<Path>, {
     }
 }
 
-fn check_positions(head: &mut (i32, i32), tail: &mut (i32, i32), visited_pos: &mut HashSet<(i32,i32)>) {
-    if ((head.0- tail.0).abs() > 1 && (head.1 - tail.1).abs() > 0) || ((head.0- tail.0).abs() > 0 && (head.1 - tail.1).abs() > 1) {
-        // tester le sens enfin
-        if head.0 > tail.0 { //RIGHT
-            tail.0 += 1;
+fn check_positions(knots: &mut Vec<(i32, i32)>, visited_pos: &mut HashSet<(i32,i32)>) {
+
+
+    for i in 0..knots.len() - 1 {
+        // if knots[i+1].0 
+        if (knots[i].0 - knots[i+1].0) .abs() > 1 || (knots[i].1 - knots[i+1].1) .abs() > 1 {
+            knots[i + 1].0 += (knots[i].0 - knots[i+1].0).signum(); 
+            knots[i + 1].1 += (knots[i].1 - knots[i+1].1).signum(); 
         }
-        else { // LEFT
-            tail.0 -= 1;
-        }
-        if head.1 > tail.1 { // UP
-            tail.1 += 1;
-        }
-        else { // DOWN
-                tail.1 -= 1;
-            }
-        visited_pos.insert(tail.clone());
+        
     }
-    if (head.0 - tail.0).abs() > 1 {
-        if head.0 > tail.0 { //RIGHT
-            tail.0 += 1;
-        }
-        else { // LEFT
-            tail.0 -= 1;
-        }
-        println!("MOVE head: {:?} TAIL: {:?}", &head, &tail);  
-    }
-    visited_pos.insert(tail.clone());
-    if (head.1 - tail.1).abs() > 1 {
-        if head.1 > tail.1 { // UP
-            tail.1 += 1;
-        }
-        else { // DOWN
-            tail.1 -= 1;
-        }
-        println!("MOVE head: {:?} TAIL: {:?}", &head, &tail);  
-    }
-    visited_pos.insert(tail.clone());
+    visited_pos.insert(knots[knots.len() -1]);
+    println!("{:?}", knots[knots.len() -1]);
+
+
+    // for (i, _) in knots.iter().enumerate() {
+    //     if i < knots.len() - 1 {
+
+    //         if ((knots[i].0- knots[i+1].0).abs() > 1 && (knots[i].1 - knots[i+1].1).abs() > 0) || ((knots[i].0- knots[i+1].0).abs() > 0 && (knots[i].1 - knots[i+1].1).abs() > 1) {
+    //             // tester le sens enfin
+    //             if knots[i].0 > knots[i+1].0 { //RIGHT
+    //                 knots[i+1].0 += 1;
+    //             }
+    //             else { // LEFT
+    //                 knots[i+1].0 -= 1;
+    //             }
+    //             if knots[i].1 > knots[i+1].1 { // UP
+    //                 knots[i+1].1 += 1;
+    //             }
+    //             else { // DOWN
+    //                     knots[i+1].1 -= 1;
+    //                 }
+    //             visited_pos.insert(knots[i+1].clone());
+    //         }
+    //         if (knots[i].0 - knots[i+1].0).abs() > 1 {
+    //             if knots[i].0 > knots[i+1].0 { //RIGHT
+    //                 knots[i+1].0 += 1;
+    //             }
+    //             else { // LEFT
+    //                 knots[i+1].0 -= 1;
+    //             }
+    //             println!("MOVE knots[i]: {:?} knots[i+1]: {:?}", &knots[i], &knots[i+1]);  
+    //         }
+    //         visited_pos.insert(knots[i+1].clone());
+    //         if (knots[i].1 - knots[i+1].1).abs() > 1 {
+    //             if knots[i].1 > knots[i+1].1 { // UP
+    //                 knots[i+1].1 += 1;
+    //             }
+    //             else { // DOWN
+    //                 knots[i+1].1 -= 1;
+    //             }
+    //             println!("MOVE knots[i]: {:?} knots[i+1]: {:?}", &knots[i], &knots[i+1]);  
+    //         }    
+    //         if i == knots.len() - 1{
+    //             visited_pos.insert(knots[i+1].clone());            
+    //         }
+    //     }
+
+    // } // end for
+
 }
 
-fn parse_ropes(s: String, head: &mut (i32, i32), tail: &mut (i32, i32), visited_pos: &mut HashSet<(i32,i32)>) {
+fn parse_ropes(s: String, knots: &mut Vec<(i32, i32)>, visited_pos: &mut HashSet<(i32,i32)>) {
     let sp: Vec<&str> = s.split(" ").collect();
     let nb: u32 = sp[1].parse().unwrap(); // if it's a number
     // up down Y, right left X
@@ -75,59 +97,59 @@ fn parse_ropes(s: String, head: &mut (i32, i32), tail: &mut (i32, i32), visited_
     for _ in 0..nb {
         match sp[0] { // direction
             "U" => {
-                    head.1 += 1;
-                    println!("Up: HEAD: {:?} tail: {:?}", &head, &tail);  
+                    knots[0].1 += 1;
+                    println!("Up: HEAD: {:?}", &knots.first().unwrap());  
             },
             "D" => { 
-                    head.1 -= 1;
-                    println!("Down: HEAD: {:?} tail: {:?}", &head, &tail);  
+                    knots[0].1 -= 1;
+                    println!("Down: HEAD: {:?}", &knots[0]);  
             },
             "L" => {
-                head.0 -= 1;
-                println!("Left: HEAD: {:?} tail: {:?}", &head, &tail);  
+                knots[0].0 -= 1;
+                println!("Left: HEAD: {:?}", &knots[0]);  
             },
             "R" => { 
-                head.0 += 1;
-                println!("Right: HEAD: {:?} tail: {:?}", &head, &tail);  
+                knots[0].0 += 1;
+                println!("Right: HEAD: {:?}", &knots[0]);  
             },
             _ => {
                 panic!("wtf");
             }
         }
-        check_positions(head, tail, visited_pos);
+        check_positions(knots, visited_pos);
     }
 }   
 
 #[test]
-fn test_parse() {
+fn test_parse1() {
     let expected: u32 = 13;
-    let mut head: (i32, i32) = (0,0);
-    let mut tail: (i32, i32) = (0,0);
+    let mut knots: Vec<(i32, i32)> = vec![(0,0),(0,0)];
     let mut visited_pos: HashSet<(i32, i32)> = HashSet::new();
     if let Ok(lines) = read_lines("test") {
         for l in lines.flatten() {
-            parse_ropes(l, &mut head, &mut tail, &mut visited_pos)
+            parse_ropes(l, &mut knots, &mut visited_pos)
+        }
+    }
+    println!("res: {}", visited_pos.iter().len() );// p1: number of visited pos by the tail
+    
+    let res = visited_pos.iter().len() as u32;
+    assert_eq!(expected, res);
+}
+
+#[test]
+fn test_parse() {
+    let expected: u32 = 36;
+    let mut knots: Vec<(i32, i32)> = vec![(0,0);10];
+    let mut visited_pos: HashSet<(i32, i32)> = HashSet::new();
+    if let Ok(lines) = read_lines("test2") {
+        for l in lines.flatten() {
+            parse_ropes(l,  &mut knots, &mut visited_pos);
+            // rendre générique parse_ropes en parcourant pour chaque noeud de la corde, genre 
+            // on lui envoie noeud et noeud.next et ça fait tout les mouvements pour chaque noeud de la corde. 
+            // parse_ropes(l, &mut head, &mut tail, &mut visited_pos)
         }
     }
     println!("res: {}", visited_pos.iter().len() );// p1: number of visited pos by the tail
     let res = visited_pos.iter().len() as u32;
     assert_eq!(expected, res);
 }
-
-// #[test]
-// fn test_parse() {
-//     let expected: u32 = 13;
-//     let mut head: (i32, i32) = (0,0);
-//     let mut tail: (i32, i32) = (0,0);
-//     let mut visited_pos: HashSet<(i32, i32)> = HashSet::new();
-//     if let Ok(lines) = read_lines("test2") {
-//         for l in lines.flatten() {
-//             // rendre générique parse_ropes en parcourant pour chaque noeud de la corde, genre 
-//             // on lui envoie noeud et noeud.next et ça fait tout les mouvements pour chaque noeud de la corde. 
-//             // parse_ropes(l, &mut head, &mut tail, &mut visited_pos)
-//         }
-//     }
-//     println!("res: {}", visited_pos.iter().len() );// p1: number of visited pos by the tail
-//     let res = visited_pos.iter().len() as u32;
-//     assert_eq!(expected, res);
-// }
